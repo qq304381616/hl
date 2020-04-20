15f0c3b6fef5fbfe48366e44fe429e952fc26d27
hl <304381616@qq.com>
2020-04-20 18:23:50 +0800

    增加集合类

--- knowledge/src/main/java/com/hl/knowledge/AndroidMapActivity.java
+++ knowledge/src/main/java/com/hl/knowledge/AndroidMapActivity.java
@@ -2,7 +2,6 @@ package com.hl.knowledge;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.hl.base.BaseActivity;
@@ -19,13 +18,16 @@ public class AndroidMapActivity extends BaseActivity {
         setContentView(tv);
 
         StringBuilder sb = new StringBuilder();
-        sb.append("HashMap<String, String>" +"\n" );
-        sb.append("arrayMap<String, String>" +"\n" );
-        sb.append("supportArrayMap<String, String>" +"\n" );
-        sb.append("simpleArrayMap<String, String>" +"\n" );
-        sb.append("SparseArray<String>" +"\n" );
-        sb.append("LongSparseArray<String>" +"\n" );
-        sb.append("SparseLongArray<String>" +"\n" );
+        sb.append("HashMap<String, String>" + "\n");
+        sb.append("arrayMap<String, String>" + "\n");
+        sb.append("supportArrayMap<String, String>" + "\n");
+        sb.append("simpleArrayMap<String, String>" + "\n");
+        sb.append("LongSparseArray<String> == List<Long, String>" + "\n");
+        sb.append("SparseLongArray == List<Integer, Long>" + "\n");
+        sb.append("SparseArray<String> == List<Long, String>" + "\n");
+        sb.append("SparseIntArray == List<Integer, Integer>" + "\n");
+        sb.append("SparseLongArray == List<Integer, Long>" + "\n");
+        sb.append("SparseBooleanArray == List<Integer, Boolean>" + "\n");
 
         tv.setText(sb.toString());
 
@@ -62,6 +64,5 @@ public class AndroidMapActivity extends BaseActivity {
 //        android.util.SparseLongArray sparseLongArray = new android.util.SparseLongArray(16);
 //        sparseLongArray.put(10, 100L);
 //        sparseLongArray.get(10);
-
     }
 }
